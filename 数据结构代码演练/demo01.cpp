bbae9d96944dbb233ac85e4cbaca654ddfc38d1d
WinStarry <xuechao.yang@foxmail.com>
2020-11-19 18:27:30 +0800

    update data

--- 数据结构代码演练/demo01.cpp
+++ 数据结构代码演练/demo01.cpp
@@ -1,5 +1,6 @@
 #include<cstdio>
 
+//按顺序打印从1到N的全部正整数（使用for循环的方式） 
 void printN(int N){
 		int i;
 		for(i=1; i<=N; i++){
@@ -7,11 +8,23 @@ void printN(int N){
 		}
 		return;
 	}
-	
+
+
+//顺序打印从1到M的全部正整数（使用递归的方式）
+void printM(int M){
+	if(M){
+	 printM(M - 1);
+	 printf("%d\n", M);
+    }
+	return;
+}	
+
 int main(){	
+
 	printN(5);
 	
 	return 0;
 }
 
+// 解决问题方法的效率跟空间的利用效率有关。
 
