0da345017f84444a38e00c26387ded5d57046430
WinStarry <xuechao.yang@foxmail.com>
2021-01-19 08:23:39 +0800

    update data

--- c语言基础/未命名1.cpp
+++ c语言基础/未命名1.cpp
@@ -1,9 +1,15 @@
 #include<cstdio>
 int main(){
 	
-	int a, b;
-	scanf("%d%d", &a, &b);
-	printf("%d", a + b);
+//	int a, b;
+//	scanf("%d%d", &a, &b);
+//	printf("%d", a+b);
+//	printf("%c", 7);
+	
+//	char arr1[25] = "wo ai de ren bu ai wo";
+	char arr1[255] = "wo zai yi de ren shi wo gan dao hen shi wang";
+	char arr2[25] = "so sad story it is.";
+	printf("%s, %s", arr1, arr2);
 	
 	return 0;
 }
