955253d4db2d21a99af630d89ceb6b8b10b9eaf7
WinStarry <xuechao.yang@foxmail.com>
2021-02-07 08:52:58 +0800

    update data

---
+++ c语言基础/冒泡排序示例.cpp
@@ -0,0 +1,22 @@
+#include<cstdio>
+
+int main(){
+	
+	int a[5] = {3, 1, 4, 5, 2};
+	
+	for(int i=1; i<=4; i++){
+		for(int j=0; j<5-i; j++){
+			if(a[j]>a[j+1]){
+				int temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+	
+	for(int i=0; i<5; i++){
+		printf("%d\n",a[i]);
+	}
+	
+	return 0;
+}
