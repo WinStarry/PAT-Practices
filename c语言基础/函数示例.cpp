992c28b1423bb387524e1edd31f243e2f1cb246d
WinStarry <xuechao.yang@foxmail.com>
2021-02-07 18:36:00 +0800

    update data

---
+++ c语言基础/函数示例.cpp
@@ -0,0 +1,20 @@
+#include<cstdio>
+
+void print1(){
+		printf("hello ");
+		printf("world");
+	} 
+	
+void print2(){
+		printf("good ");
+		printf("idea!");
+	}
+
+int main(){
+	
+	print1();
+	printf("\n"); 
+	print2();
+	
+	return 0;
+}
